1fc9f088560080efb5e5c0961442188e2d4f5983
sh3ifu <denysdatskov201@gmail.com>
2026-03-27 17:31:52 +0200

    deleted Tron directory

--- tron/Bridge/migrations/2_deploy_contracts_bridge.js
+++
@@ -1,5 +0,0 @@
-var Bridge = artifacts.require("./Bridge.sol");
-
-module.exports = function(deployer, network, accounts) {
-  deployer.deploy(Bridge);
-};
--- tron/Bridge/migrations/5_deploy_contracts_proxyAdmin.js
+++
@@ -1,5 +0,0 @@
-var BridgeContractProxyAdmin = artifacts.require("./BridgeContractProxyAdmin.sol");
-
-module.exports = function(deployer) {
-    deployer.deploy(BridgeContractProxyAdmin);    
-};
